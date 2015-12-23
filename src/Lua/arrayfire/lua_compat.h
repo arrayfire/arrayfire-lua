8623f3e5a908b57e0bddab47effbd75a987a93e7
Pavan Yalamanchili <pavan@arrayfire.com>
2015-12-22 22:32:16 -0500

    Fixing build issues on Linux / OSX

--- src/Lua/arrayfire/lua_compat.h
+++ src/Lua/arrayfire/lua_compat.h
@@ -4,17 +4,5 @@ extern "C" {
 }
 
 #if LUA_VERSION_NUM > 501
-
 #define lua_objlen(L,i)		lua_rawlen(L, (i))
-
-#define luaL_register(L, n, l) do {             \
-        lua_getglobal(L, n);                    \
-        if (lua_isnil(L, -1)) {                 \
-            lua_pop(L, 1);                      \
-            lua_newtable(L);                    \
-        }                                       \
-        luaL_setfuncs(L, l, 0);                 \
-        lua_setglobal(L, n);                    \
-    }while(0)
-
 #endif
