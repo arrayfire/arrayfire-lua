47be09431c2411482c6999d42f87b3e21428ca2d
Shehzan Mohammed <shehzan@arrayfire.com>
2015-12-23 15:04:01 -0500

    Building Lua (static) as external project

---
+++ CMakeModules/build_lua.cmake
@@ -0,0 +1,41 @@
+INCLUDE(ExternalProject)
+
+SET(prefix ${CMAKE_BINARY_DIR}/third_party/lua)
+
+SET(lua_location "${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}lua${CMAKE_STATIC_LIBRARY_SUFFIX}")
+IF(CMAKE_VERSION VERSION_LESS 3.2)
+    IF(CMAKE_GENERATOR MATCHES "Ninja")
+        MESSAGE(WARNING "Building forge with Ninja has known issues with CMake older than 3.2")
+    endif()
+    SET(byproducts)
+ELSE()
+    SET(byproducts BYPRODUCTS ${lua_location})
+ENDIF()
+
+# FIXME Tag forge correctly during release
+ExternalProject_Add(
+    lua-ext
+    GIT_REPOSITORY https://github.com/LuaDist/lua.git
+    GIT_TAG 5.3.2
+    PATCH_COMMAND patch -p1 -t -N < ${CMAKE_MODULE_PATH}/lua_MSVC.patch
+    PREFIX "${prefix}"
+    INSTALL_DIR "${prefix}"
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" <SOURCE_DIR>
+    -DCMAKE_SOURCE_DIR:PATH=<SOURCE_DIR>
+    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    -DBUILD_SHARED_LIBS=OFF
+    -DLUA_BUILD_AS_DLL=OFF
+    ${byproducts}
+    )
+
+ExternalProject_Get_Property(lua-ext install_dir)
+ADD_LIBRARY(lua IMPORTED STATIC)
+SET_TARGET_PROPERTIES(lua PROPERTIES IMPORTED_LOCATION ${lua_location})
+ADD_DEPENDENCIES(lua lua-ext)
+SET(LUA_INCLUDE_DIRECTORIES ${install_dir}/include)
+SET(LUA_LIBRARIES lua)
+SET(LUA_FOUND ON)
