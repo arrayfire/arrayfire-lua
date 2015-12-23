d251e721749168038131f4dfbd58021c65b754fe
Pavan Yalamanchili <pavan@arrayfire.com>
2015-12-22 22:08:53 -0500

    BUGFIX: Fixing export symbol for windows

--- src/Lua/arrayfire/export.h
+++ src/Lua/arrayfire/export.h
@@ -1,5 +1,5 @@
 #if defined(_WIN32) || defined(_MSC_VER)
-#define __EXPORT__ extern "C"  __declspec(dllimport)
+#define __EXPORT__ extern "C"  __declspec(dllexport)
 #else
 #define __EXPORT__ extern "C" __attribute__((visibility("default")))
 #endif
