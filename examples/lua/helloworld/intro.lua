b73162290dffeb6436facc4c6f81ed2bd0010c8c
Pavan Yalamanchili <pavan@arrayfire.com>
2015-12-22 22:56:01 -0500

    Adding a simple example snippet -- intro.lua

---
+++ examples/lua/helloworld/intro.lua
@@ -0,0 +1,19 @@
+--[[
+/*******************************************************
+ * Copyright (c) 2014, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+ --]]
+
+local AF = require("af_lib")
+
+AF.main(function()
+      local x = AF.randu(5, "f32")
+      AF.print("x", x)
+      AF.print("min of x", AF.min(x))
+      AF.print("max of x", AF.max(x))
+end)
